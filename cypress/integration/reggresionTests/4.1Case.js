c3c5c04ddd9394e8c6b9b6bfd495ccd99db129cd
geakajis <geakajis@gmail.com>
2021-10-20 10:18:32 +0200

    updateTests

--- cypress/integration/reggresionTests/4.1Case.js
+++ cypress/integration/reggresionTests/4.1Case.js
@@ -3,7 +3,7 @@ describe("Cetvrti TestCase", function () {
     it("Check email address field validation - negative ", function () {
         el.homepage.goToLoginPage();
         cy.get("#create-account_form").should("be.visible");
-        cy.get(el.homepage.input.emailInput).type("abc.def@mail#archive.com ");
+        cy.get(el.homepage.input.emailInput).type("abc.def@mail#archive.com");
         cy.get(el.homepage.button.createAnAccount).click();
         cy.get(".alert").contains("Invalid email address.").should("be.visible");
       });
--- cypress/integration/reggresionTests/7Case.js
+++ cypress/integration/reggresionTests/7Case.js
@@ -2,10 +2,10 @@ import * as el from "../pages";
 import faker from "faker";
 const email2 = faker.internet.email()
 describe("Sedmi TestCase", function () {
-  it("Check email address field validation - positive ", function () {
+  it("Validate email field locking", function () {
     l.homepage.goToLoginPage();
     el.homepage.validateMandatoryFields(email2);
     cy.get(el.homepage.input.email).should('have.value', email2);
-    cy.get(el.homepage.input.email).should("be.enabled"); // PROMENI NA BE.DISABLED
+    cy.get(el.homepage.input.email).should("be.disabled"); // PROMENI NA BE.DISABLED
   });
 });
